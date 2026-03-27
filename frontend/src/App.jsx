73a010dab18061a5a164da5fc780f9de4f1d9d1e
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2026-03-27 19:11:34 -0400

    Replaced previous front-end system with a React-powered frontend interface

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import MainContainer from './container'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <><MainContainer/></>
+  )
+}
+
+export default App
---
+++ frontend/src/container.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+
+
+function MainContainer() {
+    const [url, setUrl] = useState('');
+
+    function saveUrl() {
+        console.log(url);
+    }
+
+    return (
+        <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+            <div className="bg-white rounded-2xl shadow-lg p-8 w-80">
+                <h1 className="font-bold text-center text-2xl mb-10">Shortify</h1>
+                <input
+                    value={url}
+                    onChange = {(e) => setUrl(e.target.value)}
+                    placeholder = "Enter a URL."
+                    className="w-full border border-gray-800 rounded-lg px-4 py-2"
+                ></input>
+                <button onClick={saveUrl}
+                    className="w-full bg-gray-100 rounded-2xl mt-6 py-2 font-semibold text-black transition hover:bg-blue-500">
+                    Submit</button>
+            </div>
+        </div>
+    );
+};
+
+export default MainContainer;
