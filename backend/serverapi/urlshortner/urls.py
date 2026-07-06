28f292472cba944bbfaada2bcae4f7b6655c3d3b
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2026-07-05 20:09:58 -0400

    Set up basic routes

--- backend/serverapi/urlshortner/urls.py
+++ backend/serverapi/urlshortner/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.health_check, name='healt_check')
+    path('', views.health_check, name='health_check'),
+    path('save', views.save_url, name='save_url'),
+    path('<str:code>', views.get_url, name='get_url')
 ]
--- backend/serverapi/urlshortner/views.py
+++ backend/serverapi/urlshortner/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+
+import json
 
 # Create your views here.
 
 def health_check(request):
-    return HttpResponse('All clear! Server is running. :)')
+    return HttpResponse('All clear! Server is running. :)', status=200)
+
+# REPLACE CSRF_EXEMPT WITH A MORE SECURE ANNOTATION
+@csrf_exempt
+def save_url(request):
+    if request.method != 'POST':
+        return HttpResponse('Bad request', status=400)
+    
+    body = json.loads(request.body)
+
+    url = body['url']
+
+    return HttpResponse(url, status=200)
+
+@csrf_exempt
+def get_url(request, code):
+    return HttpResponse(code, status=200)
