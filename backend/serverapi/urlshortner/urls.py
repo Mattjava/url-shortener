5291c8e0b26f402cf2be50b19211ff35d7bc7802
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2026-07-11 18:11:35 -0400

    Added redirecting functionality to the backend server

--- backend/serverapi/urlshortner/urls.py
+++ backend/serverapi/urlshortner/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.health_check, name='health_check'),
     path('save', views.save_url, name='save_url'),
-    path('<str:code>', views.get_url, name='get_url')
+    path('<str:code>', views.get_and_redirect_url, name='get_and_redirect_url')
 ]
